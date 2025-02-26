6971e7db5a18f135bec2d2eb0e8ce116bc998438
doktorpuss <lazylarrry2392003@gmail.com>
2025-02-26 16:39:01 +0700

    up

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 const FILE_PATH = path.join(__dirname, "data", "data.txt");
-const remoteFilePath = "/home/pi3/Documents/example/data.txt";
+const remoteFilePath = "/home/pi/Documents/example/data.txt";
 
 // Kết nối SSH khi server khởi động
 connectSSH().catch(err => console.error("SSH Connection Failed:", err));
@@ -42,7 +42,7 @@ app.post("/write-and-upload", async (req, res) => {
     await uploadFile(FILE_PATH, remoteFilePath);
     console.log(`🚀 File uploaded to Raspberry Pi: ${remoteFilePath}`);
 
-    const command = `cd /home/pi3/Documents/example && sudo python data.txt`;
+    const command = `cd /home/pi/Documents/example && sudo python data.txt`;
     const output = await execCommand(command);
     console.log(`📟 Command output: ${output}`);
 
